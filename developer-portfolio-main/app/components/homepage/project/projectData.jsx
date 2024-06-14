d78697f51c5b7f553456e32e5321dcc86e8bdb87
shamyugtha-kr <shamyugthakr@gmail.com>
2024-06-14 17:17:20 +0530

    sonar

--- developer-portfolio-main/app/components/homepage/project/projectData.jsx
+++ developer-portfolio-main/app/components/homepage/project/projectData.jsx
@@ -2,7 +2,7 @@ import CCTV_Img from "../../../../public/png/projimage/cctv.jpg"
 import Ecom_Img from "../../../../public/png/projimage/ecom.jpeg"
 import DRDO_Img from "../../../../public/png/projimage/drdo.png"
 import Fraud_Img from "../../../../public/png/projimage/fraudDetect.jpeg"
-import Image from "next/image"
+
 
 export const ProjectData = [
     {
@@ -20,7 +20,7 @@ export const ProjectData = [
         id: 2,
         title: 'Fraud Detection using LSTM and AutoEncoders in Banking Sector',
         description: "Utilizing LSTM and AutoEncoders neural network technology for AI training, the Identity Theft Detection Model improves identity validation by combining behavioral analysis, document authentication, and biometric verification methods.",
-        url: 'https://dev.to/said7388/top-10-react-js-interview-questions-48dc',
+        url: '',
         cover_image: Fraud_Img,
         techStack: ["Python", "Neural Networks", "LSTM", "AutoEncoders"],
       
@@ -30,7 +30,7 @@ export const ProjectData = [
         id: 3,
         title: 'Inter-connected Wearable Safety Band',
         description: "Ultra-Wideband (UWB) technology is used in the Interconnected Wearable Safety Band project to improve the safety of Indian Navy weather deck workers. It develops a wearable gadget to send out notifications and precisely locate workers in emergency situations, such as man-over-board scenarios, as part of the DRDO IDEX competition.",
-        url: 'https://dev.to/said7388/top-10-react-js-interview-questions-48dc',
+        url: '',
         cover_image: DRDO_Img,
         techStack: ["UWB network", "Thread Mesh Network"],
       
@@ -40,7 +40,7 @@ export const ProjectData = [
         id: 4,
         title: 'CCTV Metadata Generator AI',
         description: "JavaScript is used for summary conversion in the Automatic Object Detection and Summarization project, whereas the YOLOv8 model is used for accurate object detection. By producing succinct textual summaries with efficiency, it simplifies data analysis procedures.",
-        url: 'https://dev.to/said7388/top-10-react-js-interview-questions-48dc',
+        url: '',
         cover_image: CCTV_Img,
         techStack: ["Python", "YOLOv8"],
       
